5396c432497adb4d25ac94d4031f1569d45493f5
David Moreira <david.moreira.fr@gmail.com>
2014-08-02 12:51:24 +0200

    Extend erc configuration.
    * conf/erc.el: Add image, twitter, and youtube management.

--- conf/erc.el
+++ conf/erc.el
@@ -4,12 +4,21 @@
 
 (require-or-install 'erc)
 (require 'erc-tex) ;; Local package
+(require 'erc-youtube)
+(require-or-install 'erc-image)
+(require-or-install 'erc-tweet)
+
 (erc-tex-mode)
 (erc-smiley-mode)
 (setq erc-log-channels-directory "~/.erc/logs/")
 (setq erc-save-buffer-on-part t)
 (setq erc-hide-timestamps nil)
 (setq erc-log-insert-log-on-open nil)
+
+(add-to-list 'erc-modules 'image)
+(add-to-list 'erc-modules 'tweet)
+(add-to-list 'erc-modules 'youtube)
+; (erc-update-modules)
 
 (defun erc-mode-hook-fct()
   (erc-log-mode)
